23516938ac5fd7a0ba348a8e6088e8bd751f8d17
ILLUSIONack <usman-ack@hotmail.com>
2019-02-03 21:59:38 +0100

    spinner

--- app/src/main/java/com/example/usmansiddiqui/diceapplication/Main2Activity.java
+++ app/src/main/java/com/example/usmansiddiqui/diceapplication/Main2Activity.java
@@ -4,18 +4,30 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class Main2Activity extends AppCompatActivity {
     private Button DieBtn;
+    private ImageView spinnerImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+        spinnerImage = findViewById(R.id.SpinnerImage);
+
         DieBtn = findViewById(R.id.DieBtn);
 
+        spinnerImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                spin();
+            }
+        });
         DieBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -23,7 +35,10 @@ public class Main2Activity extends AppCompatActivity {
             }
         });
     }
-
+    public void spin(){
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.anim);
+        spinnerImage.startAnimation(animation);
+    }
     public void backToDie(){
         Intent intent = new Intent(this,MainActivity.class);
         startActivity(intent);
