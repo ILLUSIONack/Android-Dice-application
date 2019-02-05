b47b503932a41d964184064c86ad0204f205a6a0
ILLUSIONack <usman-ack@hotmail.com>
2019-02-05 12:29:27 +0100

    spinner animation test

--- app/src/main/java/com/example/usmansiddiqui/diceapplication/Main2Activity.java
+++ app/src/main/java/com/example/usmansiddiqui/diceapplication/Main2Activity.java
@@ -6,12 +6,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.util.Random;
 
 public class Main2Activity extends AppCompatActivity {
     private Button DieBtn;
     private ImageView spinnerImage;
+    private Random rng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +24,6 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         spinnerImage = findViewById(R.id.SpinnerImage);
-
         DieBtn = findViewById(R.id.DieBtn);
 
         spinnerImage.setOnClickListener(new View.OnClickListener() {
@@ -36,9 +40,30 @@ public class Main2Activity extends AppCompatActivity {
         });
     }
     public void spin(){
-        Animation animation = AnimationUtils.loadAnimation(this,R.anim.anim);
-        spinnerImage.startAnimation(animation);
+        RotateAnimation rotate = new RotateAnimation(0,360,
+                RotateAnimation.RELATIVE_TO_SELF, 0.5f,RotateAnimation.RELATIVE_TO_SELF,0.5f);
+        rotate.setDuration(1000);
+        rotate.setFillAfter(true);
+        rotate.setInterpolator(new DecelerateInterpolator());
+        rotate.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        spinnerImage.startAnimation(rotate);
     }
+    
     public void backToDie(){
         Intent intent = new Intent(this,MainActivity.class);
         startActivity(intent);
